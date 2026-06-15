f5e2045b4cd8ac4f77921354cebdd26a7accb3d2
Sanio Frederic <saniofrederic2623@gmail.com>
2026-06-15 18:51:11 +0530

    Added deployment configuration

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,3 +149,9 @@ SPECTACULAR_SETTINGS = {
     '1.0.0',
 
 }
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+ALLOWED_HOSTS = ['*']
